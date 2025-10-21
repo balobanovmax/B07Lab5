7b8e84f43806c360449656a68618e3c2d5bf7824
root <root@216506.fornex.cloud>
2025-10-21 19:43:36 +0300

    Rectangle Class File

---
+++ Rectangle.java
@@ -0,0 +1,30 @@
+package lab5;
+
+public class Rectangle {
+	Point A;
+	Point B;
+	Point C;
+	Point D;
+	
+	public Rectangle(Point A, Point B, Point C, Point C) {
+		this.A = A;
+		this.B = B;
+		this.C = C;
+		this.D = D;
+	}
+	
+	public double perimeter() {
+		return A.distance(B) + B.distance(C) + C.distance(D) + D.distance(A);
+	}
+	
+	public boolean isCube() {
+		double dAB = A.distance(B);
+		double dBC = B.distance(C);
+		double dCD = C.distance(D);
+		double dDA = D.distance(A);
+		if(dAB==dBC && dBC==dCA && dCA==dCD && dCD == dDA)
+			return true;
+		else
+			return false;
+	}
+}
